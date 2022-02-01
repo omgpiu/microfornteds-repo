6582de54af80ef7a0c47c716da2aafa255efd233
a.dubrovskii <a.dubrovskii@sbereducation.ru>
2022-02-01 21:38:21 +0300

    connect Container and Marketing

---
+++ mfp/packages/container/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import  { mount as Marketing} from 'marketing/Marketing'
+
+const App = () => {
+    return <div>
+        <h1>Hey there</h1>
+    </div>
+}
+
+export default App
